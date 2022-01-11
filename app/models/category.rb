09edb1b74dd3a8d9caf0c05e0c5211ff22ef05b3
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2022-01-12 02:47:15 +0500

    Add validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many :actions
+
+  validates :name, presence: true
+  validates :icon, presence: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :actions
+  has_many :categories
+
+  validates :name, presence: true
 end
