a57a8227697795dce477396f8510f41cc346f823
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2022-01-12 02:57:14 +0500

    Make a linter check

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :join_table_category_activities
-  resources :category_ations
   resources :actions
   resources :categories
   resources :users
+
+  root 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
